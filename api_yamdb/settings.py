8a845f033f74e1a7c62f4ab4ec44e29f9fb41cd4
Contrigra <gaponov60b@gmail.com>
2020-11-28 13:43:42 +0300

    Fixed vulnerability

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -1,7 +1,9 @@
 import datetime
 import os
 
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,9 +23,7 @@ ALLOWED_HOSTS = ['*']
 MESSAGE_THEME = os.getenv('MESSAGE_THEME')
 MESSAGE_SENDER = os.getenv('MESSAGE_SENDER')
 
-from dotenv import load_dotenv
 
-load_dotenv()
 # Application definition
 
 INSTALLED_APPS = [
